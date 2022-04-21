0d46059d7369f06a9beeffdf6cb63a6250f62ba8
Erian Erik <erianerik@gmail.com>
2022-04-20 21:11:00 -0300

    chore: inicialização do projeto

---
+++ src/main/java/com/deverianerik/myUnsplash/MyUnsplashApplication.java
@@ -0,0 +1,13 @@
+package com.deverianerik.myUnsplash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyUnsplashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyUnsplashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deverianerik/myUnsplash/MyUnsplashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deverianerik.myUnsplash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyUnsplashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
